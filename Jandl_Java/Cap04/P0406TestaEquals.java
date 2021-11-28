aa8ea4a6be8354334bb5708692a35f71b54db997
walescko <walescko@gmail.com>
2021-11-28 09:49:18 -0300

    Revendo equals

---
+++ Jandl_Java/Cap04/P0406TestaEquals.java
@@ -0,0 +1,22 @@
+package com.company.Jandl_Java.Cap04;
+
+public class P0406TestaEquals {
+
+    public static void main(String [] args) {
+        Object a = new Object();
+        Object b = new Object();
+        Object c = b;
+
+        System.out.println("a==b " + a.equals(b) + "\tb==a" + b.equals(a));
+        System.out.println("c==b " + c.equals(b) + "\tb==c" + b.equals(c));
+        System.out.println("a==c " + a.equals(c) + "\tc==a" + c.equals(a));
+
+        String s = new String("Java");
+        String t = "Jandl";
+        String u = s;
+
+        System.out.println("s==t " + s.equals(t) + "\tt==s" + t.equals(s));
+        System.out.println("s==u " + s.equals(u) + "\tu==s" + u.equals(s));
+
+    }
+}
