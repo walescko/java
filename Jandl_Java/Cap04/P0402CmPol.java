2f62dc26f0ea1c4558ce2799b30c3c48267ca7b4
walescko <walescko@gmail.com>
2021-11-28 07:37:36 -0300

    Três subclasses que chamam a classe mãe

---
+++ Jandl_Java/Cap04/P0402CmPol.java
@@ -0,0 +1,7 @@
+package com.company.Jandl_Java.Cap04;
+
+public class P0402CmPol extends P0401Conversor {
+    public P0402CmPol() {
+        super(0.3937, 0.0);
+    }
+}
---
+++ Jandl_Java/Cap04/P0404KelvinCelsius.java
@@ -0,0 +1,7 @@
+package com.company.Jandl_Java.Cap04;
+
+public class P0404KelvinCelsius extends P0401Conversor{
+    public P0404KelvinCelsius() {
+        super(1.0, -273.0);
+    }
+}
