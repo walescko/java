eeb81270fd54feab11e178eea616374d1f74cc93
walescko <walescko@gmail.com>
2021-12-01 06:38:08 -0300

    Correção do código

--- Jandl_Java/Cap04/engine/P0414MensagemImprimivel.java
+++ Jandl_Java/Cap04/engine/P0414MensagemImprimivel.java
@@ -1,2 +1,18 @@
-package com.company.Jandl_Java.Cap04.engine;public class P0414MensagemImprimivel {
+package com.company.Jandl_Java.Cap04.engine;
+
+public class P0414MensagemImprimivel extends P0413Mensagem implements P0411Imprimivel {
+
+    public P0414MensagemImprimivel(String conteudo){
+        super(conteudo);
+    }
+
+    public void imprimir(){
+        imprimirNoConsole();
+    }
+
+    public void imprimirNoConsole(){
+        System.out.println(P0411Imprimivel.INICIO);
+        System.out.println(getConteudo());
+        System.out.println(P0411Imprimivel.FIM);
+    }
 }
