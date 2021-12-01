69e3af65433714afbb297cc62c66a13cd6ebd947
walescko <walescko@gmail.com>
2021-12-01 06:54:37 -0300

    Alteracação

--- Jandl_Java/Cap04/engine/P0416Editavel2.java
+++ Jandl_Java/Cap04/engine/P0416Editavel2.java
@@ -1,5 +1,14 @@
 package com.company.Jandl_Java.Cap04.engine;
 
 public interface P0416Editavel2 extends P0412Editavel{
+
+    static String transformar(String conteudo, int shift){
+        char[] array = conteudo.toCharArray();
+        for (int p=0; p<array.length; p++){
+            array[p] = (char)((array[p] + shift)>122 ? 65 + shift++ : array[p] + shift);
+        }
+        return new String(array);
+    }
+
     public void anexar (String conteudo);
 }
