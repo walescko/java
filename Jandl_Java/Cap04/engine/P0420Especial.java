2d0c891fa63f6e47b4fd77dbcc0879b120491a39
walescko <walescko@gmail.com>
2021-12-05 07:35:50 -0300

    Special or Empty Interface

---
+++ Jandl_Java/Cap04/engine/P0420Especial.java
@@ -0,0 +1,6 @@
+package com.company.Jandl_Java.Cap04.engine;
+
+public interface P0420Especial {
+
+    //interface especial ou vazia
+}
---
+++ Jandl_Java/Cap04/engine/P0421MensagemEditavel2.java
@@ -0,0 +1,7 @@
+package com.company.Jandl_Java.Cap04.engine;
+
+public class P0421MensagemEditavel2 extends P0415MensagemEditavel implements P0420Especial{
+    public P0421MensagemEditavel2(String conteudo){
+        super(conteudo);
+    }
+}
