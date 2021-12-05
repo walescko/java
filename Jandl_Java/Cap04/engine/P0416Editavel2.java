6952a2a9c0abbaf40a8860569cedb6f0501224fc
walescko <walescko@gmail.com>
2021-12-04 22:11:54 -0300

    métodos privados

--- Jandl_Java/Cap04/engine/P0416Editavel2.java
+++ Jandl_Java/Cap04/engine/P0416Editavel2.java
@@ -2,12 +2,16 @@ package com.company.Jandl_Java.Cap04.engine;
 
 public interface P0416Editavel2 extends P0412Editavel{
 
-    static String transformar(String conteudo, int shift){
+    static String transformar(String conteudo, int shift) {
         char[] array = conteudo.toCharArray();
+        return new String(deslocamento(array, shift));
+    }
+
+    public static char[] deslocamento(char[] array, int shift){
         for (int p=0; p<array.length; p++){
             array[p] = (char)((array[p] + shift)>122 ? 65 + shift++ : array[p] + shift);
         }
-        return new String(array);
+        return array;
     }
 
     public void anexar (String conteudo);
