696ba72428fe307ffc698daa5fa2ee8a70696a7a
walescko <walescko@gmail.com>
2021-12-06 07:59:22 -0300

    Outra Externa

---
+++ Jandl_Java/Cap04/P0428OutraExterna.java
@@ -0,0 +1,12 @@
+package com.company.Jandl_Java.Cap04;
+
+public class P0428OutraExterna {
+
+    private static int valor;
+
+    public static class AninhadaEstatica{
+        public void exibir(){
+            System.out.println("valor = "+ valor);
+        }
+    }
+}
