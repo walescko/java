3d18fdb458a509e38aa541a2a768df976ced92a9
walescko <walescko@gmail.com>
2021-12-06 07:54:48 -0300

    Inner class

---
+++ Jandl_Java/Cap04/P0427EmMetodo.java
@@ -0,0 +1,30 @@
+package com.company.Jandl_Java.Cap04;
+
+public class P0427EmMetodo {
+
+    public static void main(String[] args) {
+
+        Extra extra = new Extra();
+        extra.info();
+        extra.operacao(33);
+        extra.info();
+    }
+}
+
+    class Extra{
+        private int total = 0;
+        public void operacao(int v){
+            class Somador{
+                public void soma (int v){
+                    total += v;
+                }
+            }
+            Somador s = new Somador();
+            s.soma(v);
+        }
+        public void info(){
+            System.out.printf("total = " + total);
+        }
+    }
+
+
