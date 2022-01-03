e85b60053835d44ada407372ebde119ab61294f2
walescko <walescko@gmail.com>
2022-01-02 21:17:19 -0300

    Exemplo 6 do capitulo 6

---
+++ Manzano_Java/cap06/Cap06_Ex06.java
@@ -0,0 +1,17 @@
+package com.company.Manzano_Java.cap06;
+
+import java.util.Locale;
+
+public class Cap06_Ex06 {
+    public static void main(String[] args){
+        String texto = "Computador";
+
+        System.out.println();
+        System.out.println(texto.length());
+        System.out.println(texto.toLowerCase());
+        System.out.println(texto.toUpperCase());
+        System.out.println(texto.substring(0, 3));
+        System.out.println(texto.substring(3, 7));
+        System.out.println(texto.substring(7, 10));
+    }
+}
