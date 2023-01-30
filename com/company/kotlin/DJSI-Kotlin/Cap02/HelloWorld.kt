d434d58702c71d73121cfdb2f5f3014c3784ebab
Walescko <walescko@gmail.com>
2023-01-29 23:45:44 -0300

    kt - hello world

---
+++ com/company/kotlin/DJSI-Kotlin/Cap02/HelloWorld.kt
@@ -0,0 +1,5 @@
+package com.company.kotlin.`SSSB-Kotlin`.`DJSI-Kotlin`.Cap02
+
+fun main (args: Array<String>){
+    println("Hello World! Dmitry and Svetlena")
+}
---
+++ kotlin/DJSI/cap02/helloWorld.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>){
+    println("Hello World Dmitry and Svetlana")
+}
