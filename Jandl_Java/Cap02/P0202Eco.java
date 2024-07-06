ed5490b8d04ee6e7cdf2696d3b2b08f8084f39a6
walescko <walescko@gmail.com>
2024-07-06 17:44:37 -0300

    java - comented

--- Jandl_Java/Cap02/P0202Eco.java
+++ Jandl_Java/Cap02/P0202Eco.java
@@ -1,11 +1,11 @@
-package com.company.Jandl_Java.Cap02;
+//package com.company.Jandl_Java.Cap02;
 
 public class P0202Eco {
 	
 	public static void main(String args[]) {
 		for (int i = 0; i<args.length; i++) {
 			System.out.print(args[i] + " ");
-			System.out.println();
 		}
+		System.out.println();
 	}
 }
